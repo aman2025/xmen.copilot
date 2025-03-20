54c31cec8d302074fb6cdd8213e5957bbbf21adf
aman2025 <2247367564@qq.com>
2025-03-20 16:42:51 +0800

    agent: 在规则中添加一个说明，如果content包含sucess，自动执行下一步工具

--- agent/rules/instance.js
+++ agent/rules/instance.js
@@ -3,4 +3,5 @@ export const VERIFICATION_RULES = `
   - get_instances requires valid serviceId from previous get_services call
   - start_instance requires valid instanceId from previous get_instances call
   - Parameters should be consistent with previous context when part of a multi-step flow
+  - if content contains "success", go to next step
   `
