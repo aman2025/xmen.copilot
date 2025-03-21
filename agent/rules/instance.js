1f195a84029e81bdbdf7e240af0571e7593337b3
aman2025 <2247367564@qq.com>
2025-03-21 09:06:24 +0800

    agent: rule中添加success: true

--- agent/rules/instance.js
+++ agent/rules/instance.js
@@ -3,5 +3,5 @@ export const VERIFICATION_RULES = `
   - get_instances requires valid serviceId from previous get_services call
   - start_instance requires valid instanceId from previous get_instances call
   - Parameters should be consistent with previous context when part of a multi-step flow
-  - if content contains "success", go to next step
+  - if content contains "success: true", go to next step
   `
