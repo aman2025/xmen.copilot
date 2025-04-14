0d2b6c99f97d4fbfe31ae117a81cc4bc56d0d522
aman2025 <2247367564@qq.com>
2025-04-14 15:09:46 +0800

    tool_calls下index.js的移动到tools下

--- tool-calls/index.js
+++
@@ -1,6 +0,0 @@
-import get_instances from './tools/get_instances'
-import start_instance from './tools/start_instance'
-import get_services from './tools/get_services'
-
-// export a array with all the tool calls
-export const toolCalls = [get_instances, start_instance, get_services]
---
+++ tool-calls/tools/index.js
@@ -0,0 +1,6 @@
+import get_instances from './get_instances'
+import start_instance from './start_instance'
+import get_services from './get_services'
+
+// export a array with all the tool calls
+export const toolCalls = [get_instances, start_instance, get_services]
